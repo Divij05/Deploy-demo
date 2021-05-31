cab16f796c3188301f7a0d20f4ea1c5344790087
Divij05 <85099114+Divij05@users.noreply.github.com>
2021-05-31 00:32:52 -0700

    Add files via upload

---
+++ text.js
@@ -0,0 +1,124 @@
+
+/*
+This is a multiline comment
+//Author : Harry
+*/
+//document.write("Thsi is a external js2")
+//document.getElementById('th').innerHTML = "This is a para"; 
+
+var a1 = 45;
+var a11 = 5;
+var a2 = "This is me";
+//document.write(a1,a11," <br>");
+//document.write(a1+a11," <br>");
+console.log("This is connsole");
+//console.logfv is used for debugging puposes
+
+//var age = prompt("Tell me ur age");
+//alert("Your age is",age);
+/*
+document.write("5 + 9 = ", 5+9,"<br>");
+document.write("5 - 9 = ", 5-9,"<br>");
+document.write("5 * 9 = ", 5*9,"<br>");
+document.write("5 / 9 = ", 5/9,"<br>");
+document.write("5 % 9 = ", 5%9,"<br>"b );
+*/
+/*
+var foodcost = prompt("Whats is the total value");
+var no = prompt("No.of People");
+document.write("The total cost pf food was ", foodcost,"<br>");
+document.write("Each one of you has to pay ", (foodcost/no).toFixed(2),"<br>");
+*/
+/*
+document.write("2^2 = ", Math.pow(2,9),"<br>");
+document.write("abs(-34) = ", Math.abs(-34),"<br>");
+document.write("sqrt(36) = ", Math.sqrt(36),"<br>");
+*/
+/*
+var str1 = "codewithDivij";
+document.write(str1.lenght,"<br>");
+document.write(str1.indexOf("with"),"<br>");
+document.write(str1.slice(2,3),"<br>");
+*/
+//Realational Opreator : ==,!=,<,>,>=
+//Logical opreator: &&
+/*
+
+var age = prompt("what is your age");
+
+if (age<20) {
+	document.write("You go to school");
+
+}
+
+else if(age<10 || age>3)
+
+	{
+		document.write("You also go to the school");
+
+	}
+
+
+else 
+
+	{
+		document.write("Do whatever you want");
+
+	}*/
+	/*
+var age= prompt("what is you age");
+switch(age)
+{
+	case "22";
+	document.write("22 years old.. okay")
+	break;
+
+
+	case "21";
+	document.write("21 years old.. okay")
+	break;
+
+
+	case"12";
+	document.write("132 years old.. okay")
+	break;
+
+
+	default:
+	document.write("default years old... okay");
+	break;
+
+}
+
+
+
+var i=1;
+while(i<=100)
+{
+		document.write(i,"<br>");
+		i++;
+	
+}
+
+var i=1000;
+do while(i<=100)
+{
+		document.write(i,"<br>");
+		i++;
+	
+}
+
+
+for (var i=0;i<2,i++){
+	document.write(i,"<br>");
+}
+*/
+function harry()
+{
+	document.write("He iscallling harry");
+	console.log("we have executed harry function ");
+
+
+
+}
+harry();
